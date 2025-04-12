41556b3f68643ef1c9a66fd66671fe6c9d6b78ce
A7mddarwish7 <a7mddarwish7dev@gmail.com>
2025-04-13 01:58:33 +0200

    Lab 02 solution

---
+++ ITILabsSolutions/Lab 02/Lab02Solution.sql
@@ -0,0 +1,18 @@
+/*select 
+Employees.Fname +' '+Employees.Lname [FullName] , SSN , Bdate , 
+Address , Sex , Salary , Superssn , Dno from Employees*/
+
+
+--select Pname , Pnumber , Plocation , City , Dnum  from Projects
+
+--select Employees.Fname +' '+Employees.Lname [FullName] , Salary*.1 as Annual from Employees
+
+--select SSN , Employees.Fname +' '+Employees.Lname [FullName] , Salary from Employees
+--where Salary>1000;
+--select SSN , Employees.Fname +' '+Employees.Lname [FullName] , Salary from Employees
+--where Salary>10000/12;
+
+--select Employees.Fname +' '+Employees.Lname [FullName], Salary from Employees where Sex = 'F'
+
+--select Dname from Departments where MGRSSN = 968574
+--select Pname , PLocation from Projects where Dnum = 10;
